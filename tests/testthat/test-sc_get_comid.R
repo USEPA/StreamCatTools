96be857d9261ee2bf5541677b2678d28a8025046
Marc H. Weber <mweber36@gmail.com>
2022-10-21 14:29:56 -0700

    updated testing

--- tests/testthat/test-sc_get_comid.R
+++ tests/testthat/test-sc_get_comid.R
@@ -2,11 +2,13 @@ context("Test that sc_get_comid is getting NHDPlusV21 comid for location data")
 
 gages = readr::read_csv(system.file("extdata/Gages_flowdata.csv", 
                              package = "StreamCatTools"))
+gages <- gages[1:10,c('SOURCE_FEA','STATION_NM','LON_SITE','LAT_SITE')]
 test_that("st_get_comid for sample gage coordinates", {
-  gages_coms <- sc_get_comid(gages[1:10,], xcoord='LON_SITE',
-                             ycoord='LAT_SITE', crdsys=4269)
+  gages_coms <- sc_get_comid(gages, xcoord='LON_SITE',
+                             ycoord='LAT_SITE', crsys=4269)
+  gages$COMID <- strsplit(gages_coms, ",")[[1]]
   expect_true(exists("gages_coms"))
-  expect_equal(nrow(gages_coms), 10)
-  expect_equal(ncol(gages_coms), 61)
+  expect_equal(nrow(gages), 10)
+  expect_equal(ncol(gages), 5)
           })
 
