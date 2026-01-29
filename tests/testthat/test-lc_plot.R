46b5517248c17c5ff994daca1f6a4041a2b29820
Weber <Weber.Marc@epa.gov>
2026-01-28 16:21:42 -0800

    a couple updates to testing

--- tests/testthat/test-lc_plot.R
+++ tests/testthat/test-lc_plot.R
@@ -3,9 +3,9 @@ context("Test that lc_plot is creating plot object")
 
 test_that("lc_plot is producing basic NNI plot", {
   testthat::skip_on_cran()
-  p <- lc_plotnni(comid='1337420')
+  p <- lc_plotnni(comid='23794487')
   expect_true(exists("p"))
-  expect_true(class(p)[1]=="ggplot2::ggplot")
+  expect_true(class(p)[1]=="patchwork")
 })
 
 
