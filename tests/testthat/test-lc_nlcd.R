8b6e90602f6f4d7c08c80df2e7369754b37ffbab
Weber <Weber.Marc@epa.gov>
2026-02-03 09:35:17 -0800

    updates to new function syntax in testing

--- tests/testthat/test-lc_nlcd.R
+++ tests/testthat/test-lc_nlcd.R
@@ -3,7 +3,7 @@ context("Test that lc_nlcd is pulling in StreamCat API data")
 
 test_that("lc_get_data for a sample COMID returns a data frame", {
   testthat::skip_on_cran()
-  df <- lc_nlcd(year='2019', aoi='catchment',
+  df <- lc_get_nlcd(year='2019', aoi='catchment',
                 comid='23783629,23794487,23812618')
   expect_true(exists("df"))
   expect_equal(nrow(df), 3)
@@ -12,7 +12,7 @@ test_that("lc_get_data for a sample COMID returns a data frame", {
 
 test_that("lc_get_data for a county and ws metrics returns a data frame", {
   testthat::skip_on_cran()
-  df <- lc_nlcd(year='2006, 2019', aoi='watershed',
+  df <- lc_get_nlcd(year='2006, 2019', aoi='watershed',
                 comid='23794487',showAreaSqKm=TRUE, showPctFull=TRUE)
   expect_true(exists("df"))
   expect_equal(nrow(df), 1)
