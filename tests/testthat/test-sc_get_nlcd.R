ab48dbf524e01b6609e78b60df48d06d2e968084
Weber <Weber.Marc@epa.gov>
2026-09-21 16:03:52 -0700

    test error handling

--- tests/testthat/test-sc_get_nlcd.R
+++ tests/testthat/test-sc_get_nlcd.R
@@ -5,6 +5,7 @@ test_that("sc_get_nlcd for a sample COMID returns a data frame", {
   testthat::skip_on_cran()
   df <- sc_get_nlcd(year='2019', aoi='cat',
                 comid='179,1337,1337420')
+  skip_if_api_unavailable(df, "StreamCat NLCD API")
   expect_true(exists("df"))
   expect_equal(nrow(df), 3)
   expect_equal(ncol(df), 17)
@@ -14,6 +15,7 @@ test_that("sc_get_nlcd for a county and ws metrics returns a data frame", {
   testthat::skip_on_cran()
   df <- sc_get_nlcd(year='2006, 2019', aoi='ws',
                 county='41003')
+  skip_if_api_unavailable(df, "StreamCat NLCD API")
   expect_true(exists("df"))
   expect_equal(nrow(df), 632)
   expect_equal(ncol(df), 33)
--- tests/testthat/test-sc_getdata.R
+++ tests/testthat/test-sc_getdata.R
@@ -3,6 +3,7 @@ context("Test that sc_get_data is pulling in StreamCat API data")
 test_that("sc_get_data for a sample COMID returns a data frame", {
   testthat::skip_on_cran()
   df <- sc_get_data(comid='179', aoi='cat', metric='fert')
+  skip_if_api_unavailable(df, "StreamCat data API")
   expect_true(exists("df"))
   expect_equal(nrow(df), 1)
   expect_equal(ncol(df), 2)
@@ -20,6 +21,7 @@ test_that("sc_get_data for multiple COMIDs and areas and metrics returns a data 
   df <- sc_get_data(metric='pcturbmd2006,pctconif2008,rddens', 
                     aoi='cat,ws', 
                     comid='179,1337,1337420')
+  skip_if_api_unavailable(df, "StreamCat data API")
   expect_true(exists("df"))
   expect_equal(nrow(df), 3)
   expect_equal(ncol(df), 7)
@@ -29,6 +31,7 @@ test_that("sc_get_data for showAreaSqKm and showPctFull returns a data frame", {
   testthat::skip_on_cran()
   df <- sc_get_data(metric='pcturbmd2006,damdens', aoi='cat,ws', 
                     comid='179,1337,1337420',showAreaSqKm=TRUE, showPctFull=TRUE)
+  skip_if_api_unavailable(df, "StreamCat data API")
   expect_true(exists("df"))
   expect_equal(nrow(df), 3)
   expect_equal(ncol(df), 13)
@@ -38,6 +41,7 @@ test_that("sc_get_data for a hydroregion and ws metrics returns a data frame", {
   testthat::skip_on_cran()
   df <- sc_get_data(metric='pctwdwet2006', aoi='ws', 
                     region='Region01')
+  skip_if_api_unavailable(df, "StreamCat data API")
   expect_true(exists("df"))
   expect_equal(nrow(df), 65968)
   expect_equal(ncol(df), 2)
@@ -47,6 +51,7 @@ test_that("sc_get_data for a county and ws metrics returns a data frame", {
   testthat::skip_on_cran()
   df <- sc_get_data(metric='pctwdwet2006', aoi='ws', 
                     county='41003')
+  skip_if_api_unavailable(df, "StreamCat data API")
   expect_true(exists("df"))
   expect_equal(nrow(df), 632)
   expect_equal(ncol(df), 2)
@@ -55,5 +60,6 @@ test_that("sc_get_data for a county and ws metrics returns a data frame", {
 test_that("sc_get_data for all ws metrics for a COMID returns a data frame", {
   testthat::skip_on_cran()
   df <- sc_get_data(comid='1337420',metric='all', aoi='ws')
+  skip_if_api_unavailable(df, "StreamCat data API")
   expect_true(exists("df"))
 })
